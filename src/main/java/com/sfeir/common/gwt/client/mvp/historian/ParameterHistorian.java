595138d8f1769b880e3db1ac965bb080749af464
Patrice <patrice@desaintsteban.fr>
2012-12-07 01:39:03 +0100

    concat queryString when retreive the token Path

--- src/main/java/com/sfeir/common/gwt/client/mvp/historian/ParameterHistorian.java
+++ src/main/java/com/sfeir/common/gwt/client/mvp/historian/ParameterHistorian.java
@@ -1,5 +1,7 @@
 package com.sfeir.common.gwt.client.mvp.historian;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.http.client.URL;
 import com.google.gwt.user.client.History;
@@ -13,11 +15,19 @@ public class ParameterHistorian extends DefaultHistorian {
 		if (token.isEmpty()) {
 			token = Window.Location.getParameter("token");
 			if (token == null) {
-				String path = URL.decode(Window.Location.getPath());
+				String path = URL.decode(getUrl());
 				token = formater.cleanTokenPath(path);
 			}
 		}
 		GWT.log("ParameterHistorian.getToken() " + token);
 		return token;
+	}
+	
+	private String getUrl() {
+		String path = Window.Location.getPath();
+		String query = Window.Location.getQueryString();
+		if (isNullOrEmpty(query))
+			path += "?" + query;
+		return path;
 	}
 }
