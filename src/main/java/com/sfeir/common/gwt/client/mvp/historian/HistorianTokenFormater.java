f41e4d4a0330e4b57dfb64dfeea4adeb913293f8
Patrice de Saint Stéban <patrice@desaintsteban.fr>
2013-10-03 22:54:45 +0200

    Correct the HistorianTokentFormater

--- src/main/java/com/sfeir/common/gwt/client/mvp/historian/HistorianTokenFormater.java
+++ src/main/java/com/sfeir/common/gwt/client/mvp/historian/HistorianTokenFormater.java
@@ -1,13 +1,15 @@
 package com.sfeir.common.gwt.client.mvp.historian;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
 
 public class HistorianTokenFormater {
 	public String cleanTokenPath(String path) {
 		String token = "";
-		if (path != null && !path.isEmpty() && path.startsWith("/")) {
-			token = path.substring(1);
+		if (!isNullOrEmpty(path)) {
+			token = path.startsWith("/") ? path.substring(1) : path;
 		}
 		if (GWT.isClient() && !GWT.isProdMode()) { //GWT parameters
 			token = token.replaceAll("[?&]gwt\\.codesvr=([:.0-9]*)", "");
@@ -22,7 +24,7 @@ public class HistorianTokenFormater {
 		int posQuestionMark = newUri.indexOf('?');
 		if (GWT.isClient() && !GWT.isProdMode()) { //GWT parameters
 			String gwtcodesvr = Window.Location.getParameter("gwt.codesvr");
-			if (gwtcodesvr != null)
+			if (gwtcodesvr != null && !newUri.contains("gwt.codesvr"))
 				newUri += ((posQuestionMark > 0) ? "&" : "?") + "gwt.codesvr=" + gwtcodesvr;
 		}
 		return newUri;
